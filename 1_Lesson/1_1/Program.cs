5b5bff7336c71ae0611abf84594468757fac7240
Ruslan <ruslanaldarev0@gmail.com>
2022-07-15 15:12:54 +0500

    task 1_2

--- 1_Lesson/1_1/Program.cs
+++ 1_Lesson/1_1/Program.cs
@@ -1,5 +1,4 @@
 ﻿Console.WriteLine("Write a number: ");
-string s_a = Console.ReadLine ();
+int a = int.Parse(Console.ReadLine ());
 
-int a = int.Parse(s_a);
 Console.WriteLine(a * a);
---
+++ 1_Lesson/1_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine ());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine ());
+
+if (a == b * b)
+Console.WriteLine("да");
+else
+Console.WriteLine("нет");
