32f9338c56123553f222e935b0e0a70146e7b37e
Ruslan <ruslanaldarev0@gmail.com>
2022-07-16 23:18:16 +0500

    homework2

---
+++ 1_Lesson/homework2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine ());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine ());
+
+Console.WriteLine("Write a number c: ");
+int c = int.Parse(Console.ReadLine ());
+
+if (c > b)
+{
+    if (c > a)
+    {
+        Console.Write("Максимальное  - ");
+        Console.WriteLine(c);
+    }
+    else
+    {
+        Console.Write("Максимальное  - ");
+        Console.WriteLine(a);
+    }
+}
+else
+{
+    if (b > a)
+    {
+        Console.Write("Максимальное - ");
+        Console.WriteLine(b);
+    }
+    else
+    {
+        Console.Write("Максимальное - ");
+        Console.WriteLine(a);
+    }
+}
