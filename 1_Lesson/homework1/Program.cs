fc0de9cf19a0caaf1c999884bd50dd8a6db25eae
Ruslan <ruslanaldarev0@gmail.com>
2022-07-16 17:12:22 +0500

    homework1

---
+++ 1_Lesson/homework1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine ());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine ());
+
+if (a>b)
+Console.WriteLine("да");
+else 
+Console.WriteLine("нет");
+
