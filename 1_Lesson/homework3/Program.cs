9df4f9cbf9db580db930e627f2dab88da5c0c61f
Ruslan <ruslanaldarev0@gmail.com>
2022-07-19 14:40:50 +0500

    homework3

---
+++ 1_Lesson/homework3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine ());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("делится");
+}
+else
+{
+    Console.WriteLine("не делится");
+}
